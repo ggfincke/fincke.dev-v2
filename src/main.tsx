99afbf52a095cf14f20a4cf6f300d45c01ddbe57
Garrett Fincke <garrettfincke@gmail.com>
2025-10-02 23:18:44 -0400

    Add client-side routing foundation

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,24 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import ProjectsPage from './pages/ProjectsPage.tsx';
+
+const normalizePathname = (pathname: string): string =>
+{
+  if (pathname === '/')
+  {
+    return pathname;
+  }
+
+  return pathname.replace(/\/$/, '') || '/';
+};
+
+const currentPath = normalizePathname(window.location.pathname);
+
+const CurrentView = currentPath === '/projects' ? ProjectsPage : App;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <CurrentView />
   </StrictMode>
 );
